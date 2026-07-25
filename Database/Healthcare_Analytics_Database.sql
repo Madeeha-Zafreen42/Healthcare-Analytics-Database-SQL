c55cb4c9a052dfd9528764819e79967c46623a76
Madeeha Zafreen N <madeeha.business.w@gmail.com>
2026-07-25 21:50:51 +0530

    Update Healthcare_Analytics_Database.sql

--- Database/Healthcare_Analytics_Database.sql
+++ Database/Healthcare_Analytics_Database.sql
@@ -1,1 +1,65 @@
+-- Creating the Database
 
+CREATE DATABASE Healthcare_Analytics;
+USE Healthcare_Analytics;
+
+-- Creating the Tables
+
+CREATE TABLE PatientRecords (
+    PatientID INTEGER PRIMARY KEY,
+    Name TEXT,
+    Age INTEGER,
+    Gender TEXT,
+    DiabetesType TEXT,
+    LastConsultation DATE,
+    HbA1c FLOAT
+);
+
+   CREATE TABLE Patients (
+    PatientID INTEGER PRIMARY KEY,
+    FullName TEXT,
+    DateOfBirth DATE,
+    Address TEXT,
+    PhoneNumber TEXT,
+    Email TEXT,
+    MedicalHistorySummary TEXT
+);
+
+    CREATE TABLE AppointmentDetails (
+    AppointmentID INTEGER PRIMARY KEY,
+    PatientID INTEGER,
+    AppointmentDate DATE,
+    HealthcareProfessional TEXT,
+    VisitPurpose TEXT,
+    ConsultationNotes TEXT,
+    FOREIGN KEY (PatientID) REFERENCES PatientRecords(PatientID)
+);
+
+CREATE TABLE HealthcareProfessionals (
+    ProfessionalID INTEGER PRIMARY KEY,
+    Name TEXT,
+    Specialty TEXT,
+    ContactInfo TEXT
+);
+
+CREATE TABLE MedicationsPrescribed (
+ PrescriptionID INTEGER PRIMARY KEY,
+    AppointmentID INTEGER,
+    MedicationName TEXT,
+    Dosage TEXT,
+    Instructions TEXT,
+    FOREIGN KEY (AppointmentID) REFERENCES AppointmentDetails(AppointmentID)
+);
+
+CREATE TABLE Transactions (
+    TransactionID INTEGER PRIMARY KEY,
+    PatientID INTEGER,
+    TransactionDate DATE,
+    ServiceProvided TEXT,
+    AmountCharged FLOAT,
+    FOREIGN KEY (PatientID) REFERENCES PatientRecords(PatientID)
+);
+
+-- Inserting Sample Data
+
+INSERT INTO Patients (...);
